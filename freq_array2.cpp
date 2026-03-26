fe62d1bbf826b947a83000e91da71d8e50f65dc8
chiggycharms <sukhss7161@gmail.com>
2026-03-26 19:47:26 +0530

    frequency of char

--- freq_array2.cpp
+++ freq_array2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-int main(){
+/* int main(){
     string s;
     cin>>s;
     int hash[26]={0};
@@ -13,5 +13,20 @@ int main(){
         char c;
         cin>>c;
         cout<<hash[c-'a']<<endl;
+    }
+} */
+int main(){
+    string s;
+    cin>>s;
+    int hash[256]={0};
+    for(int i=0;i<s.size();i++){
+        hash[s[i]]+=1;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        cout<<hash[c]<<endl;
     }
 }
