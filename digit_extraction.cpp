70b502507a24372b73246c3278bfe08e0443ec51
chiggycharms <sukhss7161@gmail.com>
2026-03-29 08:41:39 +0530

    added counter

--- digit_extraction.cpp
+++ digit_extraction.cpp
@@ -10,10 +10,22 @@ vector<int> digit_extractor(int d){
     }
     return arr;
 }
+int digit_counter(int d){
+    int og_num=d;
+    int count=0;
+    while(og_num!=0){
+        int res=og_num%10;
+        og_num=og_num/10;
+        count++;
+    }
+    return count;
+}
 int main(){
     int x;
     cin>>x;
     for(auto x:digit_extractor(x)){
         cout<<x<<' ';
     }
+    cout<<endl;
+    cout<<digit_counter(x)<<endl;
 }
