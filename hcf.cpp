42810f4a5d0492e9aa7af4c4703c61253afbc63b
chiggycharms <sukhss7161@gmail.com>
2026-04-01 00:01:31 +0530

    hcf/hcf with euclidean algo

---
+++ hcf.cpp
@@ -0,0 +1,22 @@
+#include "bits/stdc++.h"
+using namespace std;
+int hcf(int a,int b){
+    int gcd;
+/*     for(int i=1;i<=min(a,b);i++){
+        if(a%i==0 && b%i==0){
+            hcf=i;
+        }
+    }
+    return hcf; */
+    while(a>0&&b>0){
+        if(a>b)a=a%b;
+        else b=b%a;
+    }
+    if(a==0)return b;
+    else return a;
+}
+int main(){
+    int x,y;
+    cin>>x>>y;
+    cout<<hcf(x,y)<<endl;
+}
